4111cad55ad8eb99b1a36d76e981cf52afb9fd4e
ltbProduction <49271109+ltbProduction@users.noreply.github.com>
2020-12-01 21:31:40 +0100

    Aufbau Grundgerüst Entitiy ERM, Controlloer

---
+++ Controller/ERMController.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * Class ERMController
+ * Diese Klasse ist der Controller bei der Erstellung eines ERMs.
+ */
+
+class ERMController
+{
+    /**
+     * @ERM erm
+     */
+    public $erm;
+    /**
+     * Diese Funktion erstelllt ein Modell
+     */
+    public function createModel()
+    {
+        $erm = new ERM();
+    }
+
+    /**
+     * Diese Funktion fügt eine Entität hinzu
+     */
+    public function addEntity()
+    {
+
+    }
+
+    /**
+     * Diese Funktion fügt eine Relation hinzu
+     */
+    public function addRelationship()
+    {
+
+    }
+
+    /**
+     * Diese Funktion fügt eine Generalisierung hinzu
+     */
+    public function addGeneralisation()
+    {
+
+    }
+
+    /**
+     * Diese Funktion entfernt eine Entität
+     */
+    public function removeEntity()
+    {
+
+    }
+
+    /**
+     * Diese Funktion entfernt eine Relation
+     */
+    public function removeRelationship()
+    {
+
+    }
+
+    /**
+     * Diese Funktion entfernt eine Generalisierung
+     */
+    public function removeGeneralisation()
+    {
+
+    }
+    /**
+     * Diese Funktion ändert eine Entität
+     */
+    public function changeEntity()
+    {
+
+    }
+
+    /**
+     * Diese Funktion ändert eine Relation
+     */
+    public function changeRelationship()
+    {
+
+    }
+
+    /**
+     * Diese Funktion ändert eine Generalisierung
+     */
+    public function changeGeneralisation()
+    {
+
+    }
+
+}
