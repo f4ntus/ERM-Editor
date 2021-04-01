fcc55849caa17687257ec12915bdeac202b15d5b
f4ntus <johannes@familie-scheffold.de>
2021-04-01 15:50:22 +0200

    add Navbar and Stylesheet

--- View/EditorView.php
+++ View/EditorView.php
@@ -2,9 +2,15 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Title</title>
+    <link rel="stylesheet" href="style.css">
+    <title>ERM-Editor</title>
 </head>
 <body>
-
+<ul>
+    <li><a href="about.asp">Impressum</a></li>
+    <li><a href="contact.asp">Anleitung</a></li>
+    <li><a href="news.asp">Laden</a></li>
+    <li><a href="default.asp">Speichern</a></li>
+</ul>
 </body>
 </html>
