0293814b4832ba40dfbaa7ae9f73595839f3ebe8
f4ntus <johannes@familie-scheffold.de>
2021-04-04 19:46:22 +0200

    add navbar and basic structure for the frontend

--- View/EditorView.php
+++ View/EditorView.php
@@ -6,11 +6,18 @@
     <title>ERM-Editor</title>
 </head>
 <body>
-<ul>
-    <li><a href="about.asp">Impressum</a></li>
-    <li><a href="contact.asp">Anleitung</a></li>
-    <li><a href="news.asp">Laden</a></li>
-    <li><a href="default.asp">Speichern</a></li>
-</ul>
+    <div class="grid-container">
+        <div class="headline">
+            <ul>
+                <li><a href="about.asp">Impressum</a></li>
+                <li><a href="contact.asp">Anleitung</a></li>
+                <li><a href="news.asp">Laden</a></li>
+                <li><a href="default.asp">Speichern</a></li>
+            </ul>
+        </div>
+        <div class="editor">Three</div>
+        <div class="rightMenue">Four</div>
+        <div class="outputBelow">Five</div>
+    </div>
 </body>
-</html>
+</html>^^
