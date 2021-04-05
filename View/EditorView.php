e5f349e3b10505c5eddcd1981184a03b6aec0d80
f4ntus <johannes@familie-scheffold.de>
2021-04-05 11:05:59 +0200

    add left menu

--- View/EditorView.php
+++ View/EditorView.php
@@ -15,9 +15,39 @@
                 <li><a href="default.asp">Speichern</a></li>
             </ul>
         </div>
+        <div class="leftMenue">
+            <div class="menuGroup">
+                <p>Formen</p>
+                <button class="shape" id="entity"></button>
+                <button class="shape" id="relationship"></button>
+                <button class="shape" id="isA"></button>
+            </div>
+            <div class="menuGroup">
+                <from action="">
+                    <p>Kardinalitätsrestriktionen</p>
+                    <input type="radio" id="oneNm" name="cardinality" value="oneNm">
+                    <label for="oneNm">1, n, m</label><br>
+                    <input type="radio" id="minMax" name="cardinality" value="minMax">
+                    <label for="minMax">min, max</label><br>
+                    <input type="submit" value="Aktualisieren">
+                </from>
+            </div>
+            <div class="menuGroup">
+                <div class="dropdown">
+                    <button class="dropbtn">Generalisierung</button>
+                    <div class="dropdown-content">
+                        <a href="#">Hausklassenmodell</a>
+                        <a href="#">Partionierungs-Modell</a>
+                        <a href="#">Volle Redundanz</a>
+                        <a href="#">Überrelation</a>
+                    </div>
+                </div>
+                <button>ERM umwandeln</button>
+            </div>
+        </div>
         <div class="editor">Three</div>
         <div class="rightMenue">Four</div>
         <div class="outputBelow">Five</div>
     </div>
 </body>
-</html>^^
+</html>
