28d0ba887842e0244c32c7ce4523918a8adfdede
moritzbuerkle <buerkle.moritz@web.de>
2021-04-08 15:43:17 +0200

    div component attribute with add cell function when clicked on plus button

--- View/EditorView.php
+++ View/EditorView.php
@@ -87,7 +87,42 @@
 
         <div class="row" id="idDivAddAttribute">
 
+            <h4>Zusammengesetztes Attribut Attribut hinzufügen:</h4>
+            <div class="column" style="width: 30%;">
+                <button class="button2">Zusammengesetztes<br>Attribut<br>hinzufügen</button>
+            </div>
+
+            <div class="column2" style="width: 65%;">
+                <table id="idTableCompoundAttribute" style="width:70%">
+                    <tr>
+                        <th style="text-align: center;">Oberattribut</th>
+                        <th><input placeholder="" type="text" id="idUpperAttributeName"
+                               name="idUpperAttributeName"/>
+                        </th>
+                    </tr>
+                    <tr>
+                        <td>Unterattribut</td>
+                        <td>
+                            <input placeholder="" type="text" id="idSubValueAttribute1" name="idSubValueAttribute1"/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Unterattribut</td>
+                        <td>
+                            <input placeholder="" type="text" id="idSubValueAttribute2" name="idSubValueAttribute2"/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="text-align: center;" colspan="2">
+                            <button onclick="onClickAddSubAttributeRow()" class="buttonPlus">&#43;</button>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+
         </div>
+        <hr class="hr">
+        <button style="margin:0 auto; display:block; margin-bottom: 1%; background: blue; color: white;" class="button">Fertigstellen</button>
     </div>
     <div class="outputBelow">Five</div>
 </div>
--- View/frontendScript.js
+++ View/frontendScript.js
@@ -14,8 +14,8 @@ function onClickButtonAddSimpleAttribute() {
             '            <div class="column" style="width: 30%;">\n' +
             '                <button class="button2">Einfaches<br>Attribut<br>hinzufügen</button>\n' +
             '            </div>\n' +
-            '            <div class="column2" style="width: 70%;">\n' +
-            '                <table style="width:80%;" >\n' +
+            '            <div class="column2" style="width: 60%;">\n' +
+            '                <table style="" >\n' +
             '                    <tr>\n' +
             '                        <th>Attributname</th>\n' +
             '                        <th>PK</th>\n' +
@@ -33,11 +33,11 @@ function onClickButtonAddMultiValueAttribute() {
 
     document.getElementById("idDivAddAttribute").innerHTML =
 
-        '<h4>Mehrwertiges Attribut Attribut hinzufügen:</h4>\n' +
+        '<h4>Mehrwertiges Attribut hinzufügen:</h4>\n' +
         '            <div class="column" style="width: 30%;">\n' +
         '                <button class="button2">Mehrwertiges<br>Attribut<br>hinzufügen</button>\n' +
         '            </div>\n' +
-        '            <div class="column2" style="width: 70%;">\n' +
+        '            <div class="column2" style="width: 65%;">\n' +
         '                <table style="width:70%">\n' +
         '                    <tr>\n' +
         '                        <th style="text-align: center;" >Mehrwertiges Attribut</th>\n' +
@@ -60,6 +60,50 @@ function onClickButtonAddMultiValueAttribute() {
 }
 
 function onClickButtonAddCompoundAttribute() {
+
+    document.getElementById("idDivAddAttribute").innerHTML =
+
+        '<h4>Zusammengesetztes Attribut Attribut hinzufügen:</h4>\n' +
+        '            <div class="column" style="width: 30%;">\n' +
+        '                <button class="button2">Zusammengesetztes<br>Attribut<br>hinzufügen</button>\n' +
+        '            </div>\n' +
+        '            <div class="column2" style="width: 65%;">\n' +
+        '                <table style="width:70%">\n' +
+        '                    <tr>\n' +
+        '                        <th style="text-align: center;" >Oberattribut</th>\n' +
+
+        '                    </tr>\n' +
+        '                    <tr>\n' +
+        '                        <td>\n' +
+        '                            <div class="row">\n' +
+        '\n' +
+        '                                { <input placeholder=""   type="text" id="idMultiValueAttributeName"\n' +
+        '                                       name="idMultiValueAttributeName"/> }\n' +
+        '\n' +
+        '                            </div>\n' +
+        '                        </td>\n' +
+        '\n' +
+        '                    </tr>\n' +
+        '                </table>\n' +
+        '            </div>';
+
+
+}
+
+function onClickAddSubAttributeRow(){
+
+    var table = document.getElementById("idTableCompoundAttribute");
+    var numberRows = table.rows.length;
+    alert(numberRows);
+    var row = table.insertRow(numberRows-1);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+
+    cell1.innerHTML = "Unterattribut";
+    cell2.innerHTML = "<input placeholder=\"\" type=\"text\" id=\"idSubValueAttribute\" name=\"idSubValueAttribute\"/>";
+
+
+
 
 
 }
